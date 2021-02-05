3600fdba998d1ab2df6475d81e21581a288347d2
Hugo Perlin <hugo.perlin@ifpr.edu.br>
2021-02-05 10:18:50 -0300

    entrada de dados pelo usuário

--- src/Main.java
+++ src/Main.java
@@ -5,38 +5,33 @@ public class Main {
 
 
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         String placa;
-        //String cor;
+        String cor;
         double qtdeKms;
-        //double qtdeCombustivel;
+        double qtdeCombustivel;
+        double tamanhoPortamala;
 
-        System.out.println("Digite a placa do carro:");
+
+        System.out.println("Digite a placa:");
         placa = scan.nextLine();
-
-        System.out.printf("Digite a kilometragem do carro:");
+        System.out.println("Digite a cor:");
+        cor = scan.nextLine();
+        System.out.println("Kilometragem:");
         qtdeKms = scan.nextDouble();
+        System.out.println("Quantidade de combustivel no tanque:");
+        qtdeCombustivel = scan.nextDouble();
+        System.out.println("Qual o tamanho do portamalas:");
+        tamanhoPortamala = scan.nextDouble();
 
-        //declarando
-        CarroPasseio carro;
 
-        //instanciar
-        carro = new CarroPasseio(placa,"preto",qtdeKms,15,250);
+        CarroPasseio carroPasseio = new CarroPasseio(placa,cor,qtdeKms,qtdeCombustivel,tamanhoPortamala);
 
-        System.out.println(carro.toString());
 
-        Caminhao caminhao;
-        caminhao = new Caminhao("BBB-1122","vermelho",250000,120,3,1500);
+        System.out.println(carroPasseio.toString());
 
-        System.out.println(caminhao.toString());
-
-        caminhao.carregar(2000);
-        caminhao.abastecer(50);
-
-        System.out.println(caminhao.toString());
-
-        System.out.println(caminhao instanceof Caminhao);
-        System.out.println(caminhao instanceof Veiculo);
 
     }
 
